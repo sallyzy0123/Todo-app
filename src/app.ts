a4859f9364818e043982ad379f01ed27ad87a81b
sallyzy0123 <sally_zy@protonmail.com>
2024-04-29 11:18:55 +0300

    comment the unnecessary

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../todo-app-client/build')));
+// app.use(express.static(path.join(__dirname, '../todo-app-client/build')));
 
 if (process.env.NODE_ENV === 'production') {
   // server static content
@@ -38,9 +38,9 @@ app.get<{}, MessageResponse>('/', (_req, res) => {
 
 app.use('/api/v1', api);
 
-app.get('/*', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../todo-app-client/build/index.html'));
-});
+// app.get('/*', (_req, res) => {
+//   res.sendFile(path.join(__dirname, '../todo-app-client/build/index.html'));
+// });
 
 app.use(notFound);
 app.use(errorHandler);
